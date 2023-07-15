109cd477bcf87fd6f36a449adec3b1441c75b246
upcomingtrader <contact@upcomingtrader.com>
2023-07-15 09:10:27 +0800

    updated the script to be more resilient

--- edl_2_yt.py
+++ edl_2_yt.py
@@ -1,24 +1,33 @@
 import re
-import sys
+
+def convert_to_youtube_timestamps(edl_timestamps):
+    return edl_timestamps[:8]
 
 def edl_2_yt(filename):
-    with open(filename, 'r') as f:
-        content = f.read()
+    with open(filename, 'r') as file:
+        data = file.read().replace('\r', '')
 
-    lines = content.split('\n')
+    d2 = data.replace("\n", " ")
+    d3 = d2.replace("|D:1", "|D:1\n")
+    marker_title_search = re.findall(r'\|M:(.*?)\s*\|', d3)
+    start_time = re.findall(r'\d{2}:\d{2}:\d{2}:\d{2}', d3)
+    lines = d3.split('|D:1')
+    for l in lines:
+        print(l)
 
-    chapter_titles = [re.findall(r'\|M:(.*?)\s*\|', line)[0] for line in lines if '|M:' in line]
-    timecodes = [re.findall(r'\d\d:\d\d:\d\d:\d\d', line) for line in lines if re.match(r'\d\d\d\s\s\d\d\d', line)]
+    # chapter_starts = [re.findall(r'\d\d:\d\d:\d\d:\d\d', line) for line in lines if re.search('\d\d:\d\d:\d\d:\d\d', line)]
+    chapter_starts = [convert_to_youtube_timestamps(re.findall(r'\d\d:\d\d:\d\d:\d\d', line)[0]) for line in lines if re.search('\d\d:\d\d:\d\d:\d\d', line)]
+    chapter_titles = [re.findall(r'\|M:.*', line) for line in lines if '|M:' in line]
 
-    if len(chapter_titles) == len(timecodes):
-        for i in range(len(chapter_titles)):
-            start_time = timecodes[i][0][:-3]  # Remove the frames part of the timecode
-            print(f"{start_time} - {chapter_titles[i]}") 
+    for i, start_time in enumerate(chapter_starts):
+        if i < len(chapter_titles):
+            chapter_title = chapter_titles[i][0].replace("|M:","").replace("\n","").strip()
+            # print(f"{start_time} - {chapter_titles[i]}")
+            print(f"{start_time} - {chapter_title}")
 
-if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print('Usage: python3 convert_edl_to_youtube.py filename.edl')
-        sys.exit(1)
+
     
-    filename = sys.argv[1]
-    edl_2_yt(filename)
+if __name__ == "__main__":
+    import sys
+    edl_2_yt(sys.argv[1])
+
